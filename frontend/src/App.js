531cb55bf5f4aa98ab64fd4a3d8551f0e0f0b5ce
Ismail Ali <esmailaly7400@gmail.com>
2024-05-09 02:16:01 +0300

    first update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import EditEventPage from './pages/EditEvent';
-import ErrorPage from './pages/Error';
+import EditEventPage from './pages/EditEvent'
+import ErrorPage from './pages/Error'
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
-} from './pages/EventDetail';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
-import { action as manipulateEventAction } from './components/EventForm';
-import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+} from './pages/EventDetail'
+import EventsPage, { loader as eventsLoader } from './pages/Events'
+import EventsRootLayout from './pages/EventsRoot'
+import HomePage from './pages/Home'
+import NewEventPage from './pages/NewEvent'
+import RootLayout from './pages/Root'
+import { action as manipulateEventAction } from './components/EventForm'
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter'
+
+// test comment
 
 const router = createBrowserRouter([
   {
@@ -61,10 +63,10 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+])
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
